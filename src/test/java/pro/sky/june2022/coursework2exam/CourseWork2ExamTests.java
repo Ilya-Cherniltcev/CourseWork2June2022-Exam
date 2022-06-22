023986daea4bda069dc9708ab58f642c87a2251e
Ilya Cherniltsev <cherniltcev@mail.ru>
2022-06-22 21:58:21 +0500

    first commit

---
+++ src/test/java/pro/sky/june2022/coursework2exam/CourseWork2ExamTests.java
@@ -0,0 +1,13 @@
+package pro.sky.june2022.coursework2exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ExamTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
